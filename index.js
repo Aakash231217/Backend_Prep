a337dfdbeee53e55d4c942fdc07f675d1af487f3
Aakash <rktaakash@gmail.com>
2023-12-14 02:00:06 +0530

    first commit

--- index.js
+++ index.js
@@ -2,68 +2,87 @@ import express from "express";
 import path from 'path';
 import mongoose from 'mongoose';
 import cookieParser from "cookie-parser";
+import jwt from 'jsonwebtoken';
 
-mongoose.connect("mongodb://localhost:27017/Backend",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost:27017/Backend", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.then(()=>console.log("Database Connected"))
-.catch((e)=>console.log(e));
+  .then(() => console.log("Database Connected"))
+  .catch((e) => console.log(e));
 
-const messageSchema= new mongoose.Schema({
-     name:String,
-     email:String,
-})
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
 
-const Messge= mongoose.model("Message",messageSchema);
+const User = mongoose.model("User", userSchema);
 
+const app = express();
+const users = [];
 
-const app=express();
-const users=[];
-app.use(express.static(path.join(path.resolve(),"public")));
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-
-app.get("/",(req,res)=>{
-    const {token}=req.cookies;
-    if(token){
-       res.render("logout");
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  try {
+    if (token) {
+      const decoded = jwt.verify(token, "asadwedswerdserd");
+      req.user = await User.findById(decoded._id);
+    } else {
+      return res.render("login");
     }
-    else {
-      res.render("login");
-    }
-})
-app.get("/add",(req,res)=>{
-    Messge.create({name:"Aakash", email:"rktaakash@gmail.com"}).then(()=>{
-        res.send("Nice")
-    });
-})
-app.post("/login",(req,res)=>{
-    res.cookie("token","iamin",{
-        httpOnly:true,
-        expires:new Date(Date.now() + 60*100),
-    });
-    res.redirect("/")
-})
-app.get("/success",(req,res)=>{
-    res.render("success");
-})
+  } catch (err) {
+    console.error("JWT Verification Error:", err);
+    return res.render("login");
+  }
+  next();
+};
 
-app.post("/contact",async(req,res)=>{
-    const {name, email}=req.body;
-   await Messge.create({name:name, email:email});
-   res.redirect("/success");
-   
-})
+app.get("/", isAuthenticated, (req, res) => {
+  console.log(req.user);
+  res.render("logout",{name:req.user.name});
+});
 
-app.get("/users",(req,res)=>{
-    res.json({
-        users,
-    })
-})
-app.listen(5000,()=>{
-    console.log("Server is working")
-})
+app.get("/add", (req, res) => {
+  User.create({ name: "Aakash", email: "rktaakash@gmail.com" }).then(() => {
+    res.send("Nice");
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { name, email } = req.body;
+  const user = await User.create({
+    name,
+    email,
+  });
+  const token = jwt.sign({ _id: user._id }, "asadwedswerdserd");
+  console.log(token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000), // Adjust the expiration time as needed
+  });
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()), // Set the expiration time to a date in the past
+  });
+  res.redirect("/");
+});
+
+app.get("/users", (req, res) => {
+  res.json({
+    users,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is working");
+});
